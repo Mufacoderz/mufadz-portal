b1ad24eadf18441cb3d0dee568e679fda7de9fb8
Muhammad <mf9555759@gmail.com>
2025-11-15 18:44:42 +0800

    footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,7 +2,7 @@
 
 export default function Footer() {
     return (
-        <footer className="bg-blue-700 dark:bg-blue-300 text-white dark:text-gray-700 py-3">
+        <footer className="bg-textLight dark:bg-textDark text-white dark:text-gray-700 py-3">
             <div className="max-w-6xl mx-auto px-5 flex items-center justify-center">
                 
                 <div className="text-sm font-semibold">&copy; {new Date().getFullYear()} Mufadz App</div>
