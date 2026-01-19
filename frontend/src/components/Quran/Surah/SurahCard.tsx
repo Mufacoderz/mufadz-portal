6ec6624fb711d4e4882d5ac533c0ea9349b12de7
Mufacoderz <mf9555759@gmail.com>
2026-01-19 20:46:52 +0800

    Update SurahCard.tsx

--- frontend/src/components/Quran/Surah/SurahCard.tsx
+++ frontend/src/components/Quran/Surah/SurahCard.tsx
@@ -1,15 +1,27 @@
 import { Link } from "react-router-dom";
 import type { Surah } from "../../../types/surah";
 import Number from "../Number";
+import { useEffect } from "react"
+import AOS from "aos"
+import "aos/dist/aos.css";
 
 type SurahCardProps = {
     surah: Surah;
 };
 
 function SurahCard({ surah }: SurahCardProps) {
+    useEffect(() => {
+        AOS.init({
+            duration: 1000,
+            once: false,
+            offset: 100,
+        });
+    }, []);
+    
     return (
         <Link to={`/surah/${surah.nomor}`}>
-            <div className="bg-white/80 dark:bg-gray-800/80 backdrop-blur-sm border border-blue-100 dark:border-gray-700 hover:border-blue-700 dark:hover:border-blue-100
+            <div  data-aos="fade-right" data-aos-delay={Math.random() * 200}
+                className="bg-white/80 dark:bg-gray-800/80 backdrop-blur-sm border border-blue-100 dark:border-gray-700 hover:border-blue-700 dark:hover:border-blue-100
                         rounded-2xl p-5 shadow-sm hover:shadow-md transition-all duration-300 cursor-pointer
                         hover:-translate-y-1 flex justify-between gap-3">
                 <div className="flex text-left gap-3">
@@ -27,3 +39,4 @@ function SurahCard({ surah }: SurahCardProps) {
 }
 
 export default SurahCard;
+
